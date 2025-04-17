da96c5f842ac7173dd20eef14a5c72640cfbff9a
hoh2006 <hoh2006@nyu.edu>
2025-04-17 18:54:38 -0400

    Geojson work

--- scripts.js
+++ scripts.js
@@ -44,60 +44,60 @@ map.on('load', () => {
 
 
 // loading in positions to use in a function later on
-const marker1Position = [-81.70345, 41.48479];
-const marker2Position = [-81.70439, 41.48445];
-const marker3Position = [-81.69543, 41.50863]; // Rock N Roll Hall of Fame
-const marker4Position = [-81.69368, 41.49964]; // Public Square
-const marker5Position = [-81.68080, 41.50133]; // Playhouse Square
+// const marker1Position = [-81.70345, 41.48479];
+// const marker2Position = [-81.70439, 41.48445];
+// const marker3Position = [-81.69543, 41.50863]; // Rock N Roll Hall of Fame
+// const marker4Position = [-81.69368, 41.49964]; // Public Square
+// const marker5Position = [-81.68080, 41.50133]; // Playhouse Square
 
-// name the marker 1 to know where you are
-const marker1 = new mapboxgl.Marker({ color: 'blue' })
-    .setLngLat(marker1Position)
-    .addTo(map);
+// // name the marker 1 to know where you are
+// const marker1 = new mapboxgl.Marker({ color: 'blue' })
+//     .setLngLat(marker1Position)
+//     .addTo(map);
 
-const marker2 = new mapboxgl.Marker({ color: 'green' })
-    .setLngLat(marker2Position)
-    .addTo(map);
+// const marker2 = new mapboxgl.Marker({ color: 'green' })
+//     .setLngLat(marker2Position)
+//     .addTo(map);
 
-const marker3 = new mapboxgl.Marker({ color: 'red' })
-    .setLngLat(marker3Position)
-    .addTo(map);
+// const marker3 = new mapboxgl.Marker({ color: 'red' })
+//     .setLngLat(marker3Position)
+//     .addTo(map);
 
-const marker4 = new mapboxgl.Marker({ color: 'orange' })
-    .setLngLat(marker4Position)
-    .addTo(map);
+// const marker4 = new mapboxgl.Marker({ color: 'orange' })
+//     .setLngLat(marker4Position)
+//     .addTo(map);
 
-const marker5 = new mapboxgl.Marker({ color: 'purple' })
-    .setLngLat(marker5Position)
-    .addTo(map);
+// const marker5 = new mapboxgl.Marker({ color: 'purple' })
+//     .setLngLat(marker5Position)
+//     .addTo(map);
 
-// pop ups - offset adjusts where the popup is in relation to the marker
-const popup1 = new mapboxgl.Popup({ offset: 40 }).setText('West Side Market');
+// // pop ups - offset adjusts where the popup is in relation to the marker
+// const popup1 = new mapboxgl.Popup({ offset: 40 }).setText('West Side Market');
 
-const popup2 = new mapboxgl.Popup({ offset: 40 }).setText('Great Lakes Brewing');
+// const popup2 = new mapboxgl.Popup({ offset: 40 }).setText('Great Lakes Brewing');
 
-const popup3 = new mapboxgl.Popup({ offset: 40 }).setText('Rock N Roll Hall of Fame');
+// const popup3 = new mapboxgl.Popup({ offset: 40 }).setText('Rock N Roll Hall of Fame');
 
-const popup4 = new mapboxgl.Popup({ offset: 40 }).setText('Public Square');
+// const popup4 = new mapboxgl.Popup({ offset: 40 }).setText('Public Square');
 
-const popup5 = new mapboxgl.Popup({ offset: 40 }).setText('Playhouse Square');
+// const popup5 = new mapboxgl.Popup({ offset: 40 }).setText('Playhouse Square');
 
-// setting popup to markers 1 through 5
-marker1.setPopup(popup1);
-marker2.setPopup(popup2);
-marker3.setPopup(popup3);
-marker4.setPopup(popup4);
-marker5.setPopup(popup5);
+// // setting popup to markers 1 through 5
+// marker1.setPopup(popup1);
+// marker2.setPopup(popup2);
+// marker3.setPopup(popup3);
+// marker4.setPopup(popup4);
+// marker5.setPopup(popup5);
 
-function zoomToMarker() {
-    map.easeTo({
-        center: marker1Position,
-        zoom: 16,
-        pitch: 45,
-        bearing: 0,
-        duration: 1000
-    })
-};
+// function zoomToMarker() {
+//     map.easeTo({
+//         center: marker1Position,
+//         zoom: 16,
+//         pitch: 45,
+//         bearing: 0,
+//         duration: 1000
+//     })
+// };
 
 // zoomToMarker();
 // experimenting with zoom to marker functionality
@@ -110,6 +110,59 @@ const zoominout = new mapboxgl.NavigationControl({
 
 // adding the actual buttons to the layout
 map.addControl(zoominout, 'bottom-right');
+
+const clevelandGeojsonUrl = './Cleveland_Boundary.json';
+// adding the geojson file to the map
+
+map.on('load', function () {
+    map.addSource('clevelandBoundary', {
+        type: 'geojson',
+        data: clevelandGeojsonUrl
+    });
+    // loading in the json data
+
+    map.addLayer({
+        id: 'boundsOfCleveland',
+        type: 'fill',
+        source: 'clevelandBoundary',
+        paint: {
+            'fill-color': 'steelblue',
+            'fill-opacity': 0.2
+        }
+    });
+});
+// adding the actual layer to the map itself
+
+const clevelandTifUrl = './Shore_to_Core_to_Shore_TIF_District.geojson'
+
+// adding the geojson file to the map
+
+map.on('load', function () {
+    map.addSource('clevelandTif', {
+        type: 'geojson',
+        data: clevelandTifUrl
+    });
+    // loading in the geojson data
+
+    map.addLayer({
+        id: 'ClevelandTif',
+        type: 'fill',
+        source: 'clevelandTif',
+        paint: {
+            'fill-color': 'orange',
+            'fill-opacity': 0.2
+        }
+    });
+});
+
+map.on('load', function () {
+    map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
+});
+// adding a fullscreen button to the map in the top right hand corner
+
+
+
+
 
 
 // .forEach
